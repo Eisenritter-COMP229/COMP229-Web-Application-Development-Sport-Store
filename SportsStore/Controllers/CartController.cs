823be8600d042533dfeb174cacecdafcf3c92353
George Zhou <georgezhou89@hotmail.com>
2019-10-08 16:14:25 -0400

    Chapter 9 Part 12 Displaying the Contents of the Cart

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 using SportsStore.Infrastructure;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -17,6 +18,14 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
         public RedirectToActionResult AddToCart(int productId, string returnUrl)
         {
             Product product = repository.Products
