6d4d54a17fbebfff8b0637c883967bfec1d709f0
George Zhou <georgezhou89@hotmail.com>
2019-12-04 14:23:03 -0500

    Chapter 11 Part 07 Updatiung the Product Repository

--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -14,5 +14,27 @@ namespace SportsStore.Models
             context = ctx;
         }
         public IQueryable<Product> Products => context.Products;
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                //Change product in collection
+                Product productEntry = context.Products
+                    .FirstOrDefault(p => p.ProductID == product.ProductID);
+                //Extra Precaustion
+                if (productEntry != null)
+                {
+                    productEntry.Name = product.Name;
+                    productEntry.Description = product.Description;
+                    productEntry.Price = product.Price;
+                    productEntry.Category = product.Category;
+                }
+            }
+        }
     }
 }
